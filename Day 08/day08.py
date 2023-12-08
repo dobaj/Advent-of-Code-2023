5219386296da2a4f70bebac3f61702be473eb2cb
dobaj <85978036+dobaj@users.noreply.github.com>
2023-12-08 15:43:30 -0500

    Optimized Day 8

--- Day 08/day08.py
+++ Day 08/day08.py
@@ -34,8 +34,7 @@ with open("Day 08/input.txt") as f:
     nodes = {}
     start_nodes = []
     for line in f:
-        for x in re.findall("[0-9A-Z]{2}A", line):
-            start_nodes.append([x,""])
+        start_nodes += re.findall("[0-9A-Z]{2}A", line)
         line = re.findall("[0-9A-Z]{3}",line)
         nodes[line[0]] = (line[1],line[2])
 
@@ -49,24 +48,16 @@ with open("Day 08/input.txt") as f:
         if direction == "L":
             index = 0 # left
         
-        # Run through each node
-        for j in range(len(start_nodes)):
-            start_nodes[j][1]+=start_nodes[j][0] # add to looper
-            start_nodes[j][0] = nodes[start_nodes[j][0]][index]
-        
-        remove_nodes = []
-        k = 0
-        while k < len(start_nodes):
-            string = start_nodes[k][1]
-            #The string of visited nodes is a loop if this same z 
-            # occurred a multiple of the instruction length ago
-            if len(string) % len(instructions) == 0:
-                if string[0:3] == start_nodes[k][0]:
-                    start_nodes.pop(k)
-                    loops.append(len(string)//3) #3 to account for length of node name
-            if string[2] != "Z": # Reset if there is no Z at beginning of loop
-                start_nodes[k][1] = ""
-            k+=1
+        # Run through each node. Remove once loop found
+        j = 0
+        while j < len(start_nodes):
+            start_nodes[j] = nodes[start_nodes[j]][index]
+            #Because paths start at the start of loop and only visit one z each
+            if start_nodes[j][2] == "Z":
+                loops.append(i)
+                start_nodes.pop(j)
+                continue
+            j+=1
 
     #Find the LCM of all of the loop lengths
     for i in range(len(loops)-1):
